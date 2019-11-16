a29c94025f878068c29304278b44a7b9e641f80c
lltemplar <lltemplar@github.com>
2019-11-16 19:40:53 +0800

    add velocity

--- spring-boot/src/main/java/com/example/demo/controller/HelloWorldController.java
+++ spring-boot/src/main/java/com/example/demo/controller/HelloWorldController.java
@@ -4,10 +4,20 @@ import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RestController;
 
+import java.util.Map;
+
 @RestController
 public class HelloWorldController {
     @RequestMapping(path = "/", method = RequestMethod.GET)
     public String sayHello() {
         return "Hello,World!";
+    }
+
+    @RequestMapping(path="/index",method = RequestMethod.GET)
+    public String index(Map map){
+        map.put("message", "这是测试的内容。。。");
+        map.put("toUserName", "张三");
+        map.put("fromUserName", "李四");
+        return "index";
     }
 }
